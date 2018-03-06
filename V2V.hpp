92c05090db6976b9f8f8dc1c5d24ee0fa8a8868a
gusbocju <gusbocju@student.gu.se>
2018-03-06 14:19:24 +0100

    new development branch, updated message signatures

--- V2V.hpp
+++ V2V.hpp
@@ -1,12 +1,11 @@
-static const int BROADCAST_CHANNEL = 111;
+static const int V2V_BROADCAST = 200;
 
-static const int DEMO_PLATOON_CHANNEL = 112;
+static const int DEMO_V2V_PORT = 50001;
 static const int DEMO_CAR_ID = 123;
 
-static const int ANNOUNCE_PRESENCE = 1000;
-static const int FOLLOW_REQUEST = 1001;
-static const int FOLLOW_RESPONSE = 1002;
-static const int STOP_FOLLOW = 1003;
-static const int EMERGENCY_BRAKE = 9999;
+static const int ANNOUNCE_PRESENCE = 1001;
+static const int FOLLOW_REQUEST = 1002;
+static const int FOLLOW_RESPONSE = 1003;
+static const int STOP_FOLLOW = 1004;
 static const int LEADER_STATUS = 2001;
 static const int FOLLOWER_STATUS = 3001;
